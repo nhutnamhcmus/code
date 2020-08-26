dd90d5244cc7c287284642f9c3f2439fadb5cdd0
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-08-26 23:50:50 +0700

    Update LinkedLists.js

---
+++ Javascript/data_structures/LinkedLists.js
@@ -0,0 +1,21 @@
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+  
+function LinkedList() {
+    this._length = 0;
+    this._head = null;
+}
+
+LinkedList.prototype.push = function(data) {
+
+};
+
+LinkedList.prototype.itemAt = function(index) {
+
+};
+
+LinkedList.prototype.remove = function(index) {
+
+};
