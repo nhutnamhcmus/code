c2e871aaff60f7652c9e3a4a209e8d91abeffa06
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-08-26 10:51:53 +0700

    lmao

--- C++/cpp11-example/decltype.cpp
+++ C++/cpp11-example/decltype.cpp
@@ -10,13 +10,20 @@ decltype(data->x) y;
 decltype((data->x)) z = y;
 
 // C++14
+/*
 template<typename T, typename U>
 auto add(T t, U u) { return t + u; }
-
+*/
 
 int main() {
     int i = 24;
     decltype(i) j = i * 2;
 
+    auto mul = [](int a, int b) -> int { return a * b; };
+    decltype(mul) g = mul;
+    int a = mul(3, 3);
+    int b = g(10, 2);
     std::cout << "i = " << i << ", " << "j = " << j << std::endl;
+
+    std::cout << "a = " << a << ", " << "b = " << b << std::endl;
 }
