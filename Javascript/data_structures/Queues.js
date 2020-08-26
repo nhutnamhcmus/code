56cedd6b011c9561b3a3ab7882218a9c858e6023
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-08-26 22:56:07 +0700

    Update Queues.js

---
+++ Javascript/data_structures/Queues.js
@@ -0,0 +1,39 @@
+// Define 
+function Queue() {
+    this._head = 0;
+    this._data = []; 
+}
+
+// isEmpty
+Queue.prototype.isEmpty = function() { return this._data === undefined || this._data.length == 0; };
+
+// getSize
+Queue.prototype.size = function() { return this._data.length; };
+
+// Enqueue
+Queue.prototype.enqueue = function(data) { this._data.push(data); };
+
+// Dequeue
+Queue.prototype.dequeue = function() {
+    if (this._head < 0 || 
+        this._head >= this._data.length) {
+      return null;
+    }
+    
+    var dequeuedItem = this._data[this._head];
+    this._head++;
+    
+    return dequeuedItem;
+};
+
+// Peek
+Queue.prototype.peek = function() {
+    if (this._head < 0 || 
+        this._head >= this._data.length) {
+      return null;
+    }
+    
+    return this._data[this._head];
+};
+
+let queue = new Queue();
