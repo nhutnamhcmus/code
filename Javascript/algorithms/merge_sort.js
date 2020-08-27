2865a41efb082b4ef9b84bedc4527c5f6c95230c
Le Nhut Nam <lenam.fithcmus@gmail.com>
2020-08-28 00:19:20 +0700

    Update merge_sort.js

---
+++ Javascript/algorithms/merge_sort.js
@@ -0,0 +1,37 @@
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array;
+    }
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    );
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+let arr = [6,3,5,2,4,1];
+
+let b = mergeSort(arr);
+
+console.log(b);
